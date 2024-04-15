c3234b3634499bf977587b1091feda0411a68e9e
anurpeljto <anur.peljto@stu.ibu.edu.ba>
2024-04-15 15:05:17 +0200

    test

--- api/register.js
+++ api/register.js
@@ -1,35 +1,31 @@
-const {MongoClient, ServerApiVersion} = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGODB_URI;
 const bcrypt = require('bcryptjs');
 
 const saltRounds = 10;
-const client = new MongoClient(uri);
+const client = new MongoClient(uri, { useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
-
-
-const register = async(userData) => {
-    try {
-        await client.connect();
-        const database = client.db('users-db');
-        const users = database.collection('users');
-        if(await users.findOne({email : userData.email})){
-            throw new Error(error);
-        }else {
-            bcrypt.genSalt(10, function(err, salt) {
-                bcrypt.hash(userData.password, salt, function(err, hash) {
-                    userData.password = hash;
-                    users.insertOne(userData);
-                });
-            });
-            console.log('Successfully registered');
-        }
-        
-    } catch(error){
-        throw error;
-    } finally {
-        await client.close();
+const register = async (userData) => {
+  try {
+    await client.connect();
+    const database = client.db('users-db');
+    const users = database.collection('users');
+    if (await users.findOne({ email: userData.email })) {
+      throw new Error('Email already exists');
+    } else {
+      bcrypt.genSalt(saltRounds, function (err, salt) {
+        bcrypt.hash(userData.password, salt, function (err, hash) {
+          userData.password = hash;
+          users.insertOne(userData);
+        });
+      });
+      console.log('Successfully registered');
     }
-}
+  } catch (error) {
+    throw error;
+  } finally {
+    await client.close();
+  }
+};
 
-module.exports = { register};
+module.exports = { register };
